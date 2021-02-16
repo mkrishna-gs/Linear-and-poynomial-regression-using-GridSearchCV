457b4eb59bdf077f71fcf2d16920bb901ae75b65
Murali <murali.ganesa@porsche.digital>
2021-02-16 16:09:50 +0100

    'updated code for tech talk'

--- PolyReg_GridSearchCV.py
+++ PolyReg_GridSearchCV.py
@@ -4,51 +4,31 @@ from sklearn.pipeline import make_pipeline
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
+import warnings
+warnings.filterwarnings("ignore")
+
 ### Pipeline for linear and polynomial regressions
 def PolynomialRegression(degree=2,**kwargs):
     return make_pipeline(PolynomialFeatures(degree),LinearRegression(**kwargs))
 
-def make_data(N,err=1.0,rseed=1):
-    rng = np.random.RandomState(rseed)
-    X = rng.rand(N,1) ** 2
-    y = 10 - 1. / (X.ravel() + 0.1)
-    if err > 0:
-        y += err * rng.randn(N)
-    return X,y
-
-#X,y = make_data(40)
-
 df_train = pd.read_csv('train.csv')
 df_test = pd.read_csv('test.csv')
-X = df_train['x']
-y = df_train['y']
-X_test = df_test['x']
+X_train = df_train['x'].values.reshape(-1,1)
+y_train = df_train['y']
+X_test = df_test['x'].values.reshape(-1,1)
 y_test = df_test['y']
 
-X = np.array(X)
-y = np.array(y)
-X_test = np.array(X_test)
-y_test = np.array(y_test)
-
-X = X.reshape(-1,1)
-X_test = X_test.reshape(-1,1)
 ### Intialize the hyper parameters 
 param_grid = {'polynomialfeatures__degree':np.arange(21),
               'linearregression__fit_intercept':[True,False],
               'linearregression__normalize':[True,False]}
+
 ### Find out the degree of the polynomial which provides the best fit for the given data
 grid = GridSearchCV(PolynomialRegression(),param_grid,cv=7)
-grid.fit(X,y)
+grid.fit(X_train,y_train)
 model = grid.best_estimator_
 ### Prediction using the parameters obtained from GridSearchCV
-y_pred = model.fit(X,y).predict(X_test)
+y_pred = model.fit(X_train,y_train).predict(X_test)
 print(r2_score(y_test,y_pred))
 print(grid.best_params_)
-
-
-
-
-
-
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Linear-and-poynomial-regression-using-GridSearchCV
-In this code, I have used the GridSearchCV from sci-kit learn to find the degree of the regression and also the cross vadilation method.
+In this code, the GridSearchCV from sci-kit learn is used to find the degree of the regression.
+
+pipreqs used to create the requirements.txt. Use it to share the environment. Not pip freeze which gives unwanted packages. 
